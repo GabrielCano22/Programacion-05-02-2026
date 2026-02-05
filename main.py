c7f5dce45af053ce435017d6f4f0f628fb0d7970
GabrielCano22 <gabrieljaimecanooquendo@gmail.com>
2026-02-05 11:39:08 -0500

    feat: se agrega un archivo python con la función sumar

---
+++ main.py
@@ -0,0 +1,14 @@
+def sumar_dos_numeros(numero1:int,numero2:int) ->int:
+    """
+    Docstring for sumar_dos_numeros
+    
+    :param numero1: Description
+    :type numero1: int
+    :param numero2: Description
+    :type numero2: int
+    :return: Description
+    :rtype: int
+    """
+    return numero1+numero2
+
+print(sumar_dos_numeros(2,8))
